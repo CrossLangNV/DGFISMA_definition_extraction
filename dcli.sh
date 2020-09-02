e3a340f151a6d371d340f351d34481ada6e8b97a
ArneDefauw <arne.defauw@crosslang.com>
2020-09-02 10:54:56 +0200

    update dbuild.sh dcli.sh

--- dcli.sh
+++ dcli.sh
@@ -1,2 +1,2 @@
-docker run -it --rm -p 5000:5000 --entrypoint /bin/bash -v $PWD:/train definition_extraction_app
-#docker run -it --rm -p 5000:5000 -v $PWD:/train definition_extraction_app
+#docker run -it --rm -p 5000:5000 --entrypoint /bin/bash -v $PWD:/train definition_extraction_app
+docker run -it --rm -p 5000:5000 -v $PWD:/train definition_extraction_app
