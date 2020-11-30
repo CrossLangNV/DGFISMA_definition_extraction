2d9e476e17569aa5ec7302974e9925789725882f
neuro <laurensmeeus93@gmail.com>
2020-11-30 17:11:45 +0100

    Change port to 5002 (request Oan)

--- dcli.sh
+++ dcli.sh
@@ -1,2 +1,1 @@
-#docker run -it --rm -p 5000:5000 --entrypoint /bin/bash -v $PWD:/train definition_extraction_app
-docker run -it --rm -p 5000:5000 -v $PWD:/train definition_extraction_app
+docker run -it --rm -p 5002:5000 -v $PWD:/train definition_extraction_app
